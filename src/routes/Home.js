94d2a26645d3d3278ca0e56287a9064c97b57f03
Jiwon Park <park@jiwon.me>
2021-07-19 16:23:48 +0900

    06 누이터에 사진 저장하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,62 +1,97 @@
-import React, { useEffect, useState } from 'react'
-import { dbService } from 'firebase.config'
-import Nweet from 'components/Nweet'
+import React, { useEffect, useState } from "react";
+import { dbService, storageService } from "firebase.config";
+import Nweet from "components/Nweet";
+import {v4 as uuidv4} from 'uuid';
 
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweetList, setNweetList] = useState([]);
+  const [attachment, setAttachment] = useState("");
 
-const Home = ({userObj}) => {
-    const [nweet, setNweet] = useState('')
-    const [nweetList, setNweetList] = useState([])
+  useEffect(() => {
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweetList(newArray);
+    });
+  }, []);
 
-    useEffect(() => {
-        dbService.collection('nweets').onSnapshot((snapshot) => {
-            const newArray = snapshot.docs.map((document => ({
-                id: document.id,
-                ...document.data(),
-            })))
-            setNweetList(newArray)
-        })
-    }, [])
-
-    const onSubmit = async (event) => {
-        event.preventDefault()
-        await dbService.collection('nweets').add({
-            text: nweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid
-        })
-        setNweet('')
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    let attachmentUrl = ''
+    if(attachment !== ''){
+      const attachmentRef = storageService
+        .ref()
+        .child(`${userObj.uid}/${uuidv4()}`)
+      const response = await attachmentRef.putString(attachment, 'data_url')
+      attachmentUrl = await response.ref.getDownloadURL()
     }
+    
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl
+    });
+    setNweet("");
+    setAttachment("");
+  };
 
-    const onChange = (event) => {
-        event.preventDefault()
-        const {
-            target: {value},
-        } = event
-        setNweet(value)
-    }
-    return (
-        <>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={nweet}
-                    onChange={onChange}
-                    type='text'
-                    placeholder="What's on your mind?"
-                    maxLength={120}
-                />
-                <input type='submit' value='Nweet'/>
-            </form>
-            <div>
-                {nweetList.map((nweet) => (
-                    <Nweet
-                        key={nweet.id}
-                        nweetObj={nweet}
-                        isOwner={nweet.creatorId === userObj.uid}
-                    />
-                ))}
-            </div>
-        </>
-    )
-}
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
 
-export default Home
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result)
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => setAttachment('')
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type='text'
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type='file' accept='image/*' onChange={onFileChange} />
+        <input type='submit' value='Nweet' />
+        {attachment && (<div>
+          <img src={attachment} width='50px' height='50px' />
+          <button onClick={onClearAttachment}>Clear</button>
+        </div>)}
+      </form>
+      <div>
+        {nweetList.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Home;
