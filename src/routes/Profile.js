f35235a34ca11c6e4730eb2b80bc072282ffc07c
jiwonMe <park@jiwon.me>
2021-07-19 23:30:55 +0900

    07-1 내가 쓴 트윗만 보기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import { authService } from 'firebase.config'
+import React, { useEffect } from 'react'
+import { authService, dbService } from 'firebase.config'
 import { useHistory } from 'react-router'
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const history = useHistory()
 
     const onLogOutClick = () => {
         authService.signOut()
         history.push('/')
     }
+
+    const getMyNweets = async () => {
+        const nweets = await dbService
+            .collection('nweets')
+            .where('creatorId', '==', userObj.uid)
+            .orderBy('createdAt', 'asc') //asc: 오름차순
+            .get()
+
+
+        console.log(nweets.docs.map((doc) => doc.data()))
+    }
+
+    useEffect(() => {
+        getMyNweets()
+    }, [])
+
     return (
         <>
-        <button onClick={onLogOutClick}>Log Out</button>
+            <button onClick={onLogOutClick}>Log Out</button>
         </>
     )
 }
